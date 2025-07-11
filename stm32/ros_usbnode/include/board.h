27897b3427783600a14ccd853c5c287ba145c300
Das P <palli@gmx.net>
2025-07-11 13:50:23 +0200

    slow down that machine

--- stm32/ros_usbnode/include/board.h
+++ stm32/ros_usbnode/include/board.h
@@ -48,7 +48,7 @@ extern "C"
 #define BLADEMOTOR_LENGTH_RECEIVED_MSG 16
 #define DEBUG_TYPE DEBUG_TYPE_SWO
 
-#define MAX_MPS 0.5		  // Allow maximum speed of 1.0 m/s
+#define MAX_MPS 0.2		  // Allow maximum speed of 1.0 m/s
 #define PWM_PER_MPS 300.0 // PWM value of 300 means 1 m/s bot speed so we divide by 4 to have correct robot speed but still progressive speed
 #define TICKS_PER_M 300.0 // Motor Encoder ticks per meter
 #define WHEEL_BASE  0.325		// The distance between the center of the wheels in meters
